a271fe78673aa5a523885c5892302a27ec74e589
krishna chaitanya <arekapudikrishnachaitanya@gmail.com>
2024-07-25 17:34:26 +0530

    Added password authentication and token generation

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -51,12 +51,40 @@ const userSchema = new Schema(
 
 )
 
+//pre hook is a type of method that is executed just before an event or action,
+//in this case, just before we save anything,we need to encrypt it
 userSchema.pre("save",async function(next){
     if(!this.isModified("password")) return next();
     this.password=bcrypt.hash(this.password,10)
     next()
 })
 
+userSchema.methods.isPasswordCorrect=async function(){
+    return await bcrypt.compare(password,this.password)
+}
 
+userSchema.methods.GenerateAccessToken=function(){
+   return jwt.sign({
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullname:this.fullname
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+userSchema.methods.GenerateRefreshToken=function(){
+    return jwt.sign({
+        _id:this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+}
 
 export const User = mongoose.model("User", userSchema);
