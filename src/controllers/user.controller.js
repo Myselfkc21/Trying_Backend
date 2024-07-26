7215c4f883a290eb894d07db04db109687fd12d6
krishna chaitanya <arekapudikrishnachaitanya@gmail.com>
2024-07-27 00:17:55 +0530

    register user control 50% done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,24 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-
+import {ApiError} from "../utils/ApiError.js"
+import {User} from "../models/user.model.js"
 const registerUser=asyncHandler(async(req,res)=>{
-    res.status(200).json({
-        message:"its working"
+    const {fullname,username,email,password}=req.body;
+    console.log(email);
+    if((fullname || username||email||password)==""){
+        throw new ApiError(400,"All fields are required")
+    }
+    if(!email.includes("@")){
+        throw new ApiError(400,"Email is not valid")
+    }
+
+    const existingUser=User.findOne({
+        $or:[{username},{email}]
     })
+
+    if(existingUser){
+        throw new ApiError(400,"user Alreay exists")
+    }
+
 })
 
-export default registerUser
+export default registerUser  
